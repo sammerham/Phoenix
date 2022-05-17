b6edd3b829105936fb26816829eab052fe05d35c
Sam Merham <sammerham@icloud.com>
2022-05-17 14:01:41 -0700

    added secret key

--- config.js
+++ config.js
@@ -5,9 +5,10 @@ const DB_URI = (process.env.NODE_ENV === "test")
   : process.env.DATABASE_URL || "postgresql:///phoenixdb"
 
 const PORT = process.env.PORT || 3001;
-
+const SECRET_KEY = process.env.SECRET_KEY || 'i-have-a-secret'
 
 module.exports = {
   DB_URI,
-  PORT
+  PORT,
+  SECRET_KEY
 };
